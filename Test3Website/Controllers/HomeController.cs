8f223f15ac1a54954555c1f5676d738850f345c3
pk25 <38956167+pk25@users.noreply.github.com>
2019-07-21 04:07:25 +0530

    updat4

--- Test3Website/Controllers/HomeController.cs
+++ Test3Website/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace Test3Website.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page1.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your contact page1.";
 
             return View();
         }
